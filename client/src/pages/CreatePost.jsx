af2fd9fd6a8ab90abb8f410e7c5a4ca7627b34f4
Veysel Kartalmis <veyselkartalms@gmail.com>
2023-11-25 13:30:28 +0300

    feat: the process of adding posts to the database has been completed.

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,14 +1,39 @@
 import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { Navigate } from "react-router-dom";
 
 const CreatePost = () => {
   const [title, setTitle] = useState("");
   const [summary, setSummary] = useState("");
   const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  async function createNewPost(e) {
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("conten", content);
+    data.set("file", files[0]);
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:4000/post", {
+      method: "POST",
+      body: data,
+    });
+
+    if (response.ok) {
+      setRedirect(true);
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
 
   return (
-    <form>
+    <form onSubmit={createNewPost}>
       <input
         type="text"
         placeholder="Title"
@@ -21,7 +46,7 @@ const CreatePost = () => {
         value={summary}
         onChange={(e) => setSummary(e.target.value)}
       />
-      <input type="file" />
+      <input type="file" onChange={(e) => setFiles(e.target.files)} />
       <ReactQuill value={content} onChange={setContent} />
       <button style={{ marginTop: "5px" }}>Create Post</button>
     </form>
