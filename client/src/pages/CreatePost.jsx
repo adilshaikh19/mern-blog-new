c9bf0803867e71d58d71a2c9550061e3f726d371
Veysel Kartalmis <veyselkartalms@gmail.com>
2023-11-25 11:53:45 +0300

    feat: create post page added

---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder="Summary"
+        value={summary}
+        onChange={(e) => setSummary(e.target.value)}
+      />
+      <input type="file" />
+      <ReactQuill value={content} onChange={setContent} />
+      <button style={{ marginTop: "5px" }}>Create Post</button>
+    </form>
+  );
+};
+
+export default CreatePost;
